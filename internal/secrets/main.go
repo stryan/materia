15605535bbf321b0ee124c597f96171e2e9a581d
stryan <stryan@saintnet.tech>
2025-08-16 01:54:01 -0400

    stash

--- internal/secrets/main.go
+++ internal/secrets/main.go
@@ -6,6 +6,11 @@ type SecretsConfig interface {
 	String() string
 }
 
+type Secret struct {
+	Key, Value      string
+	ContainerSecret bool
+}
+
 type SecretFilter struct {
 	Hostname  string
 	Roles     []string
